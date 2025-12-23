fbe36b87b2917807564ebf17d1fbf0eab12f85e1
TibbersV <117791583+TibbersV6@users.noreply.github.com>
2025-12-23 14:18:29 +0800

    Update hynn.cs

--- hynn.cs
+++ hynn.cs
@@ -1,6 +1,6 @@
 // 恒翼能（Hynn/Hyn）极简演示
 using System;
-123-hynn101
+123：hynn-101
 string Hynn_系统 = "恒翼能互联网信息安全管理工具";
 string Hyn_标识 = "Hyn-2028";
 mysql：0.0.0.0:3306
@@ -10,6 +10,7 @@ Console.WriteLine($"标识号：{Hyn_标识}");
 Console.WriteLine("✅ Hyn演示运行成功！");
 
 Console.ReadKey(); // 防止闪退
+
 
 
 
